a3fb168dc0d4bb67f9e6ed9d6bcec2416e7ee6c2
Edson Victor da cruz Menezes <91231848+cruzmenezes@users.noreply.github.com>
2024-12-25 17:21:30 -0300

    operadores matematicos

---
+++ python-projetos/day-1/day-2.py
@@ -0,0 +1,19 @@
+# operações matematicas
+
+num1 = 2+2 # adição
+num2 = 2-2 # subtração
+num3 = 2*2 # multiplicação
+num4 = 2/2 # divisão
+num5 = 2%2 # pocentagem
+num6 = 2**2 # potenciação
+num7 = 2//2 # resto da divisão
+
+
+
+print("A adição dos numeros é: ", num1)
+print("A subtração dos numeros é: ", num2)
+print("A multiplicação dos numeros é: ", num3)
+print("A divisão dos numeros é: ", num4)
+print("A potentagem dos numeros é: ", num5)
+print("A potenciação dos numeros é: ", num6)
+print("A resto da divisão dos numeros é: ", num7)
