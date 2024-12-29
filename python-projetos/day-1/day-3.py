71a266008af70321fd3cebb6d0d985c4649ab2c7
Edson Victor da cruz Menezes <91231848+cruzmenezes@users.noreply.github.com>
2024-12-29 15:21:35 -0300

    validação par ou impar

---
+++ python-projetos/day-1/day-3.py
@@ -0,0 +1,13 @@
+# validação par ou impar
+
+# solicitação de um numero pelo usuario
+numero = int(input("Digite um numero: "))
+
+# verificação do numero
+
+if numero % 2 == 0:
+    print(f"o numero {numero} é par.")
+else:
+    print(f"o numero {numero} é impar. ")    
+
+   
